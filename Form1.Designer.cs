eb55d8862c46dcf0988a3fa6a3db5c7b71aefee5
TAEYEON KIM <jenniferkim0704@outlook.kr>
2026-03-12 11:43:38 +0900

    기본 도망 기

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            RunningButton = new Button();
+            SuspendLayout();
+            // 
+            // RunningButton
+            // 
+            RunningButton.BackColor = Color.DeepSkyBlue;
+            RunningButton.Font = new Font("맑은 고딕", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            RunningButton.ForeColor = Color.WhiteSmoke;
+            RunningButton.Location = new Point(268, 146);
+            RunningButton.Name = "RunningButton";
+            RunningButton.Size = new Size(227, 73);
+            RunningButton.TabIndex = 0;
+            RunningButton.Text = "나를 잡아봐";
+            RunningButton.UseVisualStyleBackColor = false;
+            RunningButton.MouseEnter += RunningButton_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(RunningButton);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button RunningButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,27 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+
+        private void RunningButton_MouseEnter(object sender, EventArgs e)
+        {
+            //RunningButton.Location = new Point(x_position, y_position);
+            // 1. 난수 생성기 준비
+            Random rd = new Random();
+
+            // 2. 가용 영역 계산 (버튼이 폼 테두리에 걸리지 않게 보호)
+            //ClientSize는 타이틀 바와 테두리를 제외한 실제 흰 도화지 영역임
+            int maxX = this.ClientSize.Width;
+            int maxY = this.ClientSize.Height;
+
+            // 3. 랜덤 좌표 추출 (0 ~ 최대 가용치 사이)
+            int nextX = rd.Next(0, maxX);
+            int nextY = rd.Next(0, maxY);
+
+            // 4. 위치 할당 (새로운 Point 객체 생성)
+            RunningButton.Location = new Point(nextX, nextY);
+
+            // 5. 시각적피드백(폼제목표시줄에좌표출력)
+            this.Text = $"버튼위치: ({nextX}, {nextY})";
+        }
     }
 }
